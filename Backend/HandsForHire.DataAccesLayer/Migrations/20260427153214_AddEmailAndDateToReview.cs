8bb2e49f79e25c28ec53b278b49a148284c51a21
sutonna <adrianabobescu1@gmail.com>
2026-04-28 10:12:58 +0300

    Am extins entitatea Review cu Email și CreatedAt

---
+++ Backend/HandsForHire.DataAccesLayer/Migrations/20260427153214_AddEmailAndDateToReview.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HandsForHire.DataAccesLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailAndDateToReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Reviews",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReviewerEmail",
+                table: "Reviews",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "ReviewerEmail",
+                table: "Reviews");
+        }
+    }
+}
