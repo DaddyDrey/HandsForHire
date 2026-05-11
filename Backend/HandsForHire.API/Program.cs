930f422a049b5977a78504f274d972cd63c5fe46
Nikitos-git <nichita.toncu@isa.utm.md>
2026-05-11 22:47:03 +0300

    serialize API enums as strings

--- Backend/HandsForHire.API/Program.cs
+++ Backend/HandsForHire.API/Program.cs
@@ -2,10 +2,15 @@ using HandsForHire.DataAccesLayer.Context;
 using Microsoft.EntityFrameworkCore;
 using HandsForHire.BusinessLogic.Interfaces;
 using HandsForHire.BusinessLogic.Structure;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
