259cbc0949076455be70990907376cff15391d97
Nikitos-git <nichita.toncu@isa.utm.md>
2026-06-03 19:00:55 +0300

    Fix password hash migration metadata

--- Backend/HandsForHire.DataAccesLayer/Migrations/20260603190000_AddUserPasswordHash.cs
+++ Backend/HandsForHire.DataAccesLayer/Migrations/20260603190000_AddUserPasswordHash.cs
@@ -1,3 +1,5 @@
+using HandsForHire.DataAccesLayer.Context;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -5,6 +7,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace HandsForHire.DataAccesLayer.Migrations
 {
     /// <inheritdoc />
+    [DbContext(typeof(AppDbContext))]
+    [Migration("20260603190000_AddUserPasswordHash")]
     public partial class AddUserPasswordHash : Migration
     {
         /// <inheritdoc />
